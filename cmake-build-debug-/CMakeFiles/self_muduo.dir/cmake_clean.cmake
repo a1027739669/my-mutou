ae04b168379399c6b3701cfe60939b2e97bfc9d5
zyx <1027739669@qq.com>
2022-09-17 20:44:52 +0800

    init

---
+++ cmake-build-debug-/CMakeFiles/self_muduo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/self_muduo.dir/main.cpp.o"
+  "self_muduo"
+  "self_muduo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/self_muduo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by R oot on 2022/9/17.
+//
+#include "sys/epoll.h"
+#include "iostream"
+using namespace std;
+int main(){
+
+  cout<<"hello world";
+}
